f783313170c99e25270f72513f90bd62013695ec
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-12-13 12:34:30 +0700

    Update PayOSController.java

--- src/main/java/com/mssus/app/controller/PayOSController.java
+++ src/main/java/com/mssus/app/controller/PayOSController.java
@@ -61,12 +61,12 @@ public class PayOSController {
     public ResponseEntity<String> handleWebhook(@RequestBody String payload) {
         try {
             log.info("Received PayOS webhook");
-//            PayOSService.WebhookPayload webhookPayload = payOSService.parseWebhook(payload);
-//            topUpService.handleTopUpWebhook(
-//                    webhookPayload.orderCode(),
-//                    webhookPayload.status(),
-//                    webhookPayload.amount()
-//            );
+            PayOSService.WebhookPayload webhookPayload = payOSService.parseWebhook(payload);
+            topUpService.handleTopUpWebhook(
+                    webhookPayload.orderCode(),
+                    webhookPayload.status(),
+                    webhookPayload.amount()
+            );
             return ResponseEntity.ok("Succeeded");
         } catch (Exception e) {
             log.error("Error processing webhook", e);
